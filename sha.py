bbad5044c076e64ed7f6e9b7b2fd09555c1821cf
joeylemon <joeyclemon@gmail.com>
2021-09-20 17:04:40 -0400

    complete sha1 implementation

--- sha.py
+++ sha.py
@@ -8,55 +8,65 @@ WORD_BIT_MASK = 0xffffffff
 # The initial hash value H^0 (FIPS 180-4 5.3.1)
 IV = 0x67452301efcdab8998badcfe10325476c3d2e1f0
 
-
-def ROTL(x, n):
-    """
-    The rotate left operation, where x is a w=32-bit word
-    and n is an integer with 0 <= n < w. (FIPS 180-4 2.2.2)
-    """
-    return ((x << n)|(x >> (WORD_BIT_LENGTH - n))) & WORD_BIT_MASK
- 
-
-def ROTR(x, n):
-    """
-    The rotate right operation, where x is a w=32-bit word
-    and n is an integer with 0 <= n < w. (FIPS 180-4 2.2.2)
-    """
-    return ((x >> n)|(x << (WORD_BIT_LENGTH - n))) & WORD_BIT_MASK
+# Print intermediate values if verbose is enabled
+VERBOSE = False
 
 
-def ft(t, x, y, z):
+def sha1(msg):
     """
-    Each function ft operates on three 32-bit words, x, y, and z, 
-    and produces a 32-bit word as output. (FIPS 180-4 4.1.1)
+    Pre-process the message with padding, parse it into blocks,
+    and hash each block using hash(iv, b) where iv is the previous
+    hash value and b is the current block. (FIPS 180-4 6.1)
     """
-    if t >= 0 and t <= 19:
-        # Ch(x,y,z)
-        return (x & y) ^ (~x & z)
-    elif t >= 20 and t <= 39:
-        # Parity(x,y,z)
-        return x ^ y ^ z
-    elif t >= 40 and t <= 59:
-        # Maj(x,y,z)
-        return (x & y) ^ (x & z) ^ (y ^ z)
-    elif t >= 60 and t <= 79:
-        # Parity(x,y,z)
-        return x ^ y ^ z
+    blocks = parse(pad(msg))
+    cv = IV
+    for block in blocks:
+        cv = hash(cv, block)
+    return "{:040x}".format(cv)
 
 
-def K(t):
+def hash(iv, msg_block):
     """
-    SHA-1 uses a sequence of eighty constant 32-bit words: 
-    K0, K1,..., K79. (FIPS 180-4 4.2.1)
+    Hash the given block using the given intermediate value (IV).
+    (FIPS 180-4 6.1.2)
     """
-    if t >= 0 and t <= 19:
-        return 0x5a827999
-    elif t >= 20 and t <= 39:
-        return 0x6ed9eba1
-    elif t >= 40 and t <= 59:
-        return 0x8f1bbcdc
-    elif t >= 60 and t <= 79:
-        return 0xca62c1d6
+    h0, h1, h2, h3, h4 = extract_hex_words(iv, total_bits=160)
+    a, b, c, d, e = h0, h1, h2, h3, h4
+    W = schedule(msg_block)
+
+    __print_schedule(W)
+    __print_round_header()
+    for t in range(80):
+        T = add(ROTL(a, 5), ft(t, b, c, d), e, K(t), W[t])
+        e = d
+        d = c
+        c = ROTL(b, 30)
+        b = a
+        a = T
+        __print_round(t, a, b, c, d, e)
+
+    return create_hex_value([add(a, h0), add(b, h1), add(c, h2), add(d, h3), add(e, h4)], WORD_BIT_LENGTH)
+
+
+def __print_round(t, a, b, c, d, e):
+    """ Print the current round's values if verbose is enabled. """
+    if VERBOSE:
+        print(f"{t:<3}  {a:<08x}  {b:<08x}  {c:<08x}  {d:<08x}  {e:<08x}")
+
+
+def __print_round_header():
+    """ Print the round header if verbose is enabled. """
+    if VERBOSE:
+        print(f"\n{'t':<3}  {'a':<8}  {'b':<8}  {'c':<8}  {'d':<8}  {'e':<8}")
+
+
+def __print_schedule(W):
+    """ Print the message schedule values if verbose is enabled. """
+    if VERBOSE:
+        for i in range(0, 80, 10):
+            vals = '  '.join("{:08x}".format(w) for w in W[i:i+10])
+            W_range = f"[{i}...{i+9}]"
+            print(f"W{W_range:<9} = {vals}")
 
 
 def pad(msg):
@@ -68,7 +78,7 @@ def pad(msg):
     # Suppose that the length of the message is l bits
     l = len(msg) * 8
 
-    # k zero bits, where k is the smallest, non-negative 
+    # k zero bits, where k is the smallest, non-negative
     # solution to the equation l + 1 + k = 448 mod 512
     k = (448 - l - 1) % 512
 
@@ -76,7 +86,7 @@ def pad(msg):
     bits = int(''.join("{:08b}".format(ord(c)) for c in msg), 2)
 
     # Put the message to the far left so it can be followed
-    # by a 1-bit, k 0-bits, and 64-bits for length
+    # by a 1-bit, k 0-bits, and 64 bits for length
     bits <<= 1 + k + 64
 
     # Add the 1-bit
@@ -93,25 +103,73 @@ def parse(padded_msg):
     Parse the padded message into 512-bit blocks. (FIPS 180-4 5.2.1)
     """
     num_bits = padded_msg.bit_length()
-    num_blocks = num_bits // 512 if num_bits % 512 == 0 else (num_bits + 1) // 512
-    blocks = [(padded_msg >> (i * 512) & BIT_MASK_512) for i in range(0, num_blocks)][::-1]
-    print(num_blocks)
-    for i, block in enumerate(blocks):
-        print("\nBLOCK " + str(i))
-        print("{:0512b}".format(block))
-    return blocks
+
+    # Round num_bits to the nearest 512, then divide by 512
+    num_blocks = (512 * round(num_bits / 512)) // 512
+
+    # The blocks are the 512-bit sections of the padded message
+    return [(padded_msg >> (i * 512) & BIT_MASK_512)
+            for i in range(0, num_blocks)][::-1]
+
+
+def schedule(msg_block):
+    """
+    Build the message schedule for the given block. (FIPS 180-4 6.1.2)
+    """
+    W = extract_hex_words(msg_block, total_bits=512) + [0] * 64
+    for t in range(16, 80):
+        W[t] = ROTL(W[t-3] ^ W[t-8] ^ W[t-14] ^ W[t-16], 1)
+    return W
+
+
+def ROTL(x, n):
+    """
+    The rotate left operation, where x is a w=32-bit word
+    and n is an integer with 0 <= n < w. (FIPS 180-4 2.2.2)
+    """
+    return ((x << n) | (x >> (WORD_BIT_LENGTH - n))) & WORD_BIT_MASK
+
+
+def ft(t, x, y, z):
+    """
+    Each function ft operates on three 32-bit words, x, y, and z, 
+    and produces a 32-bit word as output. (FIPS 180-4 4.1.1)
+    """
+    if 0 <= t <= 19:
+        return (x & y) ^ (~x & z)
+    elif 20 <= t <= 39:
+        return x ^ y ^ z
+    elif 40 <= t <= 59:
+        return (x & y) ^ (x & z) ^ (y & z)
+    elif 60 <= t <= 79:
+        return x ^ y ^ z
+
+
+def K(t):
+    """
+    SHA-1 uses a sequence of eighty constant 32-bit words: 
+    K0, K1,..., K79. (FIPS 180-4 4.2.1)
+    """
+    if 0 <= t <= 19:
+        return 0x5a827999
+    elif 20 <= t <= 39:
+        return 0x6ed9eba1
+    elif 40 <= t <= 59:
+        return 0x8f1bbcdc
+    elif 60 <= t <= 79:
+        return 0xca62c1d6
 
 
 def extract_hex_words(val, total_bits):
     """
-    Given a hex value, extract it into an array of words
+    Given a hex value, extract it into an array of words.
     """
     return [(val >> (i * WORD_BIT_LENGTH)) & WORD_BIT_MASK for i in range(0, total_bits // WORD_BIT_LENGTH)][::-1]
 
 
 def create_hex_value(arr, elem_bit_size):
     """
-    Given an array of bit values, combine them into a single integer value.
+    Given an array of word values, combine them into a single integer value.
     """
     total_bits = len(arr) * elem_bit_size
     val = 0
@@ -120,72 +178,26 @@ def create_hex_value(arr, elem_bit_size):
     return val
 
 
-def add(a, b):
+def add(*args):
     """
-    Perform addition (+) modulo 2^32 (FIPS 180-4 6.1.2)
+    Perform addition (+) modulo 2^32. (FIPS 180-4 6.1.2)
     """
-    return (a + b) % 2**32
-
-
-def schedule(msg_block):
-    """
-    Build the message schedule for the given block. (FIPS 180-4 6.1.2)
-    """
-    W = extract_hex_words(msg_block, total_bits=512)
-    for t in range(16, 80):
-        W.append(ROTL(W[t-3] ^ W[t-8] ^ W[t-14] ^ W[t-16], 1))
-    return W
-    
-
-def hash(iv, msg_block):
-    """
-    Hash the given block using the given intermediate value (IV).
-    (FIPS 180-4 6.1.2)
-    """
-    h0,h1,h2,h3,h4 = extract_hex_words(msg_block, total_bits=160)
-    a,b,c,d,e = h0,h1,h2,h3,h4
-    W = schedule(msg_block)
-
-    for t in range(80):
-        T = ROTL(a, 5) + ft(t, b, c, d) + e + K(t) + W[t]
-        e = d
-        d = c
-        c = ROTL(b, 30)
-        b = a
-        a = T
-
-    return create_hex_value([add(a, h0), add(b, h1), add(c, h2), add(d, h3), add(e, h4)], WORD_BIT_LENGTH)
-
-
-def sha1(msg):
-    """
-    Pre-process the message with padding, parse it into blocks,
-    and hash each block using hash(iv, b) where iv is the previous
-    hash value and b is the current block. (FIPS 180-4 6.1)
-    """
-    blocks = parse(pad(msg))
-    cv = IV
-    for block in blocks:
-        print(hex(cv))
-        cv = hash(cv, block)
-    return cv
-
-"""
-W0, W1, ..., W79        word schedule
-a,b,c,d,e               working variables
-H^0                     initial hash value
-H^i_0, ... H^i_4        words of ith hash value
-T                       temp word
-M                       message
-N                       number of blocks
-ROTL^n(x)               circular left shift
-"""
+    return sum(args) & WORD_BIT_MASK
 
 
 if __name__ == "__main__":
-    padded_msg = pad("a" * 131)
-    blocks = parse(padded_msg)
-    print(hex(blocks[0]))
-    print(hex(hash(IV, blocks[0])))
-    print(hex(sha1("abc")))
-    print(hex(add(0xffffffff, 0x00000002)))
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        prog="sha",
+        description="Hash values using the SHA1 algorithm")
+    parser.add_argument("-v", "--verbose",
+                        action="store_true",
+                        help="enable verbose logging")
+    parser.add_argument("value",
+                        type=str,
+                        help="the value to hash")
+    args = parser.parse_args()
+    VERBOSE = args.verbose
+
+    print(sha1(args.value))
--- test.py
+++ test.py
@@ -1,52 +1,101 @@
 import unittest
+import hashlib
+import random
+import string
 import sha
+
 
 class TestUtilities(unittest.TestCase):
     def test_create_hex_value(self):
         self.assertEqual(
-            sha.create_hex_value([0xaaaaaaaa, 0xbbbbbbbb, 0xcccccccc], 32), 
+            sha.create_hex_value([0xaaaaaaaa, 0xbbbbbbbb, 0xcccccccc], 32),
             0xaaaaaaaabbbbbbbbcccccccc)
         self.assertEqual(
-            sha.create_hex_value([0xaa, 0xbb, 0xcc], 8), 
+            sha.create_hex_value([0x00010000, 0xbbbbbbbb, 0x00001000], 32),
+            0x00010000bbbbbbbb00001000)
+        self.assertEqual(
+            sha.create_hex_value([0xaa, 0xbb, 0xcc], 8),
             0xaabbcc)
         self.assertNotEqual(
-            sha.create_hex_value([0xaa, 0xbb, 0xcc], 32), 
+            sha.create_hex_value([0xaa, 0xbb, 0xcc], 32),
             0xaabbcc)
-
 
     def test_extract_hex_words(self):
         self.assertEqual(
-            sha.extract_hex_words(0xaabbccddeeffaabb, 64),
+            sha.extract_hex_words(0xaabbccddeeffaabb, total_bits=64),
             [0xaabbccdd, 0xeeffaabb])
         self.assertEqual(
-            sha.extract_hex_words(0xaaaaaaaabbbbbbbbccccccccdddddddd, 128),
+            sha.extract_hex_words(
+                0xaaaaaaaabbbbbbbbccccccccdddddddd, total_bits=128),
             [0xaaaaaaaa, 0xbbbbbbbb, 0xcccccccc, 0xdddddddd])
-
+        self.assertEqual(
+            sha.extract_hex_words(
+                0x67452301efcdab8998badcfe10325476c3d2e1f0, total_bits=160),
+            [0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476, 0xc3d2e1f0])
 
     def test_ROTL(self):
         self.assertEqual(sha.ROTL(0xaabbccdd, 4), 0xabbccdda)
         self.assertEqual(sha.ROTL(0xdeadbeef, 4), 0xeadbeefd)
         self.assertEqual(
-            sha.ROTL(0b10100000000000000000000000000001, 1), 
+            sha.ROTL(0b10100000000000000000000000000001, 1),
             0b01000000000000000000000000000011)
 
 
-    def test_ROTR(self):
-        self.assertEqual(sha.ROTR(0xaabbccdd, 4), 0xdaabbccd)
-        self.assertEqual(sha.ROTR(0xdeadbeef, 4), 0xfdeadbee)
-        self.assertEqual(
-            sha.ROTR(0b01111111111111111111111111111010, 1), 
-            0b00111111111111111111111111111101)
-
-
 class TestSHA(unittest.TestCase):
+    def get_bin(self, string):
+        """
+        Get the binary representation of the given string.
+        e.g. get_bin("ab") => "0110000101100010"
+        """
+        return ''.join(["{:08b}".format(ord(c)) for c in string])
+
+    def rand_str(self, n):
+        """ Get a random string of size n. """
+        return ''.join(random.choice(string.printable) for _ in range(n))
+
     def test_pad(self):
+        msg = self.get_bin("a")
         self.assertEqual(
             sha.pad("a"),
-            int('01100001' + '1' + '0' * 439 + '0' * 60 + '1000', 2))
+            int(msg + '1' + '0' * (512 - len(msg) - 65) + "{:064b}".format(len(msg)), 2))
+
+        msg = self.get_bin("abc")
         self.assertEqual(
             sha.pad("abc"),
-            int('011000010110001001100011' + '1' + '0' * 423 + '0' * 59 + '11000', 2))
+            int(msg + '1' + '0' * (512 - len(msg) - 65) + "{:064b}".format(len(msg)), 2))
+
+        # Since message is > 447, there should be 2 blocks of 512 bits
+        msg = self.get_bin("abc" * 19)
+        self.assertEqual(
+            sha.pad("abc" * 19),
+            int(msg + '1' + '0' * (1024 - len(msg) - 65) + "{:064b}".format(len(msg)), 2))
+
+        # Since message is > 447*2, there should be 3 blocks of 512 bits
+        msg = self.get_bin("teststring" * 12)
+        self.assertEqual(
+            sha.pad("teststring" * 12),
+            int(msg + '1' + '0' * (1536 - len(msg) - 65) + "{:064b}".format(len(msg)), 2))
+
+    def test_parse(self):
+        msg = self.get_bin("abc" * 19)
+        padded_str = msg + '1' + '0' * \
+            (1024 - len(msg) - 65) + "{:064b}".format(len(msg))
+        self.assertEqual(
+            sha.parse(int(padded_str, 2)),
+            [int(padded_str[:512], 2), int(padded_str[512:], 2)])
+
+        msg = self.get_bin("teststring" * 12)
+        padded_str = msg + '1' + '0' * \
+            (1536 - len(msg) - 65) + "{:064b}".format(len(msg))
+        self.assertEqual(
+            sha.parse(int(padded_str, 2)),
+            [int(padded_str[:512], 2), int(padded_str[512:1024], 2), int(padded_str[1024:], 2)])
+
+    def test_sha1(self):
+        for _ in range(100):
+            rand_msg = self.rand_str(100)
+            self.assertEqual(hashlib.sha1(
+                rand_msg.encode()).hexdigest(), sha.sha1(rand_msg))
 
 
 if __name__ == "__main__":
