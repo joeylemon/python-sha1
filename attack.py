99b21a32775aa25d46010d6c762dcfd513ff16a1
joeylemon <joeyclemon@gmail.com>
2021-09-21 22:36:25 -0400

    complete MAC attack

--- attack.py
+++ attack.py
@@ -25,7 +25,7 @@ def concatenate(a: int, b: int) -> int:
     return int.from_bytes(a_bytes + b_bytes, byteorder="big")
 
 
-def extend(m: str, m_malicious: str, MAC: int) -> (int, str):
+def extend(m: str, m_malicious: str, MAC: int):
     """
     Perform a message extension attack on the given message. Use the
     MAC as the initial value for SHA-1 since it has the secret baked in.
@@ -38,8 +38,8 @@ def extend(m: str, m_malicious: str, MAC: int) -> (int, str):
 
     # m' = m || padding || K || m_malicious
     m_prime = concatenate(padded_m, sha.encode_string(m_malicious))
-    __print("Mallory's calculated m_prime:")
-    __print_hex(m_prime)
+    __debug_print("Mallory's calculated m_prime:")
+    __print_hex_chars(m_prime)
 
     blocks = sha.parse(sha.pad(m_prime))
 
@@ -52,7 +52,7 @@ def extend(m: str, m_malicious: str, MAC: int) -> (int, str):
     return (slice_bytes(m_prime, KEY_SIZE), f"{cv:040x}")
 
 
-def run(S: str, m: str, m_malicious: str) -> (str, str):
+def run(S: str, m: str, m_malicious: str, MAC: str = None):
     """
     Perform a test run by comparing the HMAC of the original message m
     (e.g. HMAC(S, m) = SHA1(S || m)) with the MAC of the extended message
@@ -63,6 +63,8 @@ def run(S: str, m: str, m_malicious: str) -> (str, str):
 
     # Alice calculates MAC = SHA1(S || m)
     MAC_a = int(sha.sha1(sha.encode_string(S + m)), 16)
+    if MAC:
+        MAC_a = int(MAC, 16)
 
     # Mallory extends the message m and sends Bob m' and MAC'
     m_prime, MAC_prime = extend(m, m_malicious, MAC_a)
@@ -70,14 +72,14 @@ def run(S: str, m: str, m_malicious: str) -> (str, str):
     # Bob calculates MAC = SHA1(S || m')
     MAC_b = sha.sha1(concatenate(sha.encode_string(S), m_prime))
 
-    __print("Bob will calculate SHA1 of:")
-    __print_hex(concatenate(sha.encode_string(S), m_prime))
+    __debug_print("Bob will calculate SHA1 of:")
+    __print_hex_chars(concatenate(sha.encode_string(S), m_prime))
 
     # MAC' should be equal to Bob's MAC
     return m_prime, MAC_prime, MAC_b
 
 
-def __print_hex(val: int) -> None:
+def __print_hex_chars(val: int) -> None:
     """
     Print a value in hexadecimal with ASCII characters printed only if
     verbose logging is enabled.
@@ -92,10 +94,22 @@ def __print_hex(val: int) -> None:
         print("\n")
 
 
-def __print(*args):
+def __get_hex(val: str) -> None:
+    """ Get the hex representation of the given string. """
+    return "".join(f'{ord(c):02x}' for c in val) + f' ("{val}")'
+
+
+def __debug_print(*args):
     """ Print the given values only if verbose logging is enabled. """
     if VERBOSE:
         print(*args)
+
+
+def __print(*args, color="0"):
+    """ Print the values with the given color code. """
+    print(f"\033[{color}m", end='')
+    print(*args)
+    print("\033[0m", end='')
 
 
 if __name__ == "__main__":
@@ -116,21 +130,33 @@ if __name__ == "__main__":
     parser.add_argument("malicious",
                         type=str,
                         help="the malicious message from Mallory")
+    parser.add_argument("--mac",
+                        type=str,
+                        help="the MAC value for the given message")
     args = parser.parse_args()
     VERBOSE = args.verbose
 
-    m_prime, MAC_prime, HMAC = run(args.secret, args.message, args.malicious)
+    MAC = sha.sha1(sha.encode_string(args.secret + args.message))
+    if args.mac:
+        MAC = args.mac
 
-    print("Alice calculates MAC as SHA1(S || m)")
-    print(f"S=0x{"".join(f'{ord(c)}:02x' for c in S)}")
-    print(f"m=0x{args.message:x}")
-    print(f"MAC=0x{sha.sha1(sha.encode_string(args.secret + args.message))}")
+    m_prime, MAC_prime, HMAC = run(args.secret, args.message, args.malicious, args.mac)
 
-    print("\nMallory extends m to create m'")
-    print(f"m'=0x{m_prime:x}")
+    __print("Alice calculates MAC as SHA1(S || m)", color="1;33")
+    if not args.mac:
+        __print(f"S=   0x{__get_hex(args.secret)}")
+    __print(f"m=   0x{__get_hex(args.message)}")
+    __print(f"MAC= 0x{MAC}")
 
-    print(f"Mallory's MAC 0x{MAC_prime}")
-    print(f"Bob's MAC     0x{HMAC}")
+    __print("\nMallory extends m to create m' = m || padding || K || malicious text", color="1;33")
+    __print(f"malicious text= 0x{__get_hex(args.malicious)}")
+    __print(f"m'= 0x{m_prime:x}")
 
-    print("\nMallory sends m' to Bob")
-    print(f"m'={hex(m_prime)}")
+    __print("\nMallory calculates a new MAC' using Alice's MAC as the intermediate value to SHA1:", color="1;33")
+    __print(f"MAC'= 0x{MAC_prime}")
+
+    __print("\nMallory sends m' and MAC' to Bob", color="1;33")
+
+    if not args.mac:
+        __print("Bob calculates the MAC of m' and compares it to Mallory's MAC:", color="1;33")
+        __print(f"MAC= 0x{HMAC}")
